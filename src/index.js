f7df5ba5512ca41a94cfcbe15af8a521e198ccc9
Konstantin <you@example.com>
2021-05-10 19:05:16 +0300

    Add sass styles, fonts, images

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import ReactDOM from "react-dom";
-import App from "./components/app/app";
+import ReactDOM from 'react-dom';
+import App from './components/app/app';
+import './scss/style.scss';
 
 
 ReactDOM.render(
